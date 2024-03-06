cadcac76737f3870fdb33dd9bba30a1d212274f1
Fazi <faf2001f@gmail.com>
2024-03-06 19:00:15 +0500

    ...

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
   return (
     <div className={HeaderStyles.header}>
       <div className={HeaderStyles.logo}>
-        <img src="../../../public/assets/logo.png" alt="Logo" />
+        <img src="../../../assets/logo.png" alt="Logo" />
       </div>
       <div className={HeaderStyles.navButtons}>
         {windowWidth <= 700 ? (
